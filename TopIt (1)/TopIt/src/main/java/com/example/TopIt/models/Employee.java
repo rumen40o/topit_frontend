3d7124e78eaf0d7234b21d5448486356eec36d2f
rumen40o <rumen4o_rusev@abv.bg>
2022-12-13 11:14:20 +0200

    backedn again

--- TopIt (1)/TopIt/src/main/java/com/example/TopIt/models/Employee.java
+++ TopIt (1)/TopIt/src/main/java/com/example/TopIt/models/Employee.java
@@ -1,2 +1,25 @@
-package com.example.TopIt.models;public class Employee {
+package com.example.TopIt.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerators;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
+public class Employee {
+
+    @Id
+    @S
+    private Long id;
+    private String name;
+    private String place;
+    private String email;
+    private Team team;
+    private Task task;
+
 }
