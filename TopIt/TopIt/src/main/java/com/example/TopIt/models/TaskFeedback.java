d127b945720a3645285f667a2404242d4c1b31e2
rumen40o <rumen4o_rusev@abv.bg>
2023-04-19 22:47:14 +0300

    ебало си е майката

---
+++ TopIt/TopIt/src/main/java/com/example/TopIt/models/TaskFeedback.java
@@ -0,0 +1,34 @@
+package com.example.TopIt.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class TaskFeedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false,updatable = false)
+    private Long id;
+
+    private String content;
+
+
+    @ManyToOne(
+            fetch = FetchType.EAGER
+        )
+    @JoinColumn(
+            name = "task",
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(name = "task_id")
+    )
+    private Tasks task;
+
+}
---
+++ TopIt/TopIt/src/main/java/com/example/TopIt/repository/FeedBackRepository.java
@@ -0,0 +1,17 @@
+package com.example.TopIt.repository;
+
+import com.example.TopIt.models.TaskFeedback;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FeedBackRepository extends JpaRepository<TaskFeedback,Long> {
+
+    Optional<TaskFeedback> findTaskFeedbackById(Long id);
+
+
+
+}
